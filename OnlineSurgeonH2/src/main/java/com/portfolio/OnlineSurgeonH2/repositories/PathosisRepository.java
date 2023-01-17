ff42f5470e7299964ef2c149fa09799cb3e7fdb6
alex-strae <99674687+alex-strae@users.noreply.github.com>
2023-01-17 12:59:22 +0100

    Delete OnlineSurgeonH2 directory

--- OnlineSurgeonH2/src/main/java/com/portfolio/OnlineSurgeonH2/repositories/PathosisRepository.java
+++
@@ -1,8 +0,0 @@
-package com.portfolio.OnlineSurgeonH2.repositories;
-
-import com.portfolio.OnlineSurgeonH2.model.Pathosis;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PathosisRepository extends JpaRepository<Pathosis, Integer>{
-
-}
--- OnlineSurgeonH2/src/main/java/com/portfolio/OnlineSurgeonH2/repositories/PatientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.portfolio.OnlineSurgeonH2.repositories;
-
-import com.portfolio.OnlineSurgeonH2.model.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository<Patient, Integer> {
-        }
-
